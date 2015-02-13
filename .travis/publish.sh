5c6033321444fe78a4c1ebac415cca22af757a8c
Jorge Israel Peña <jorge.israel.p@gmail.com>
2015-02-12 21:41:46 -0800

    test travis deploy to crates.io

---
+++ .travis/publish.sh
@@ -0,0 +1,10 @@
+[ $TRAVIS_BRANCH = master ] && [ $TRAVIS_PULL_REQUEST = false ] && exit 0
+
+cargo login $CRATESIO_TOKEN
+
+if cargo publish; then
+  echo "Published new version on crates.io"
+else
+  echo "Version already existed"
+fi
+
