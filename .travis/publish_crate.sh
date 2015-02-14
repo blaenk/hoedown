cb9b416c6cb858ea27b9d503ea84007b8661bda4
Jorge Israel Peña <jorge.israel.p@gmail.com>
2015-02-14 06:39:39 -0800

    identify myself with git

--- .travis/publish_crate.sh
+++ .travis/publish_crate.sh
@@ -12,6 +12,9 @@ if cargo publish; then
   echo "Creating new git tag"
   version=$(cat Cargo.toml | sed -ne 's/version = "\(.*\)"/\1/p')
 
+  git config user.name "Jorge Israel Peña"
+  git config user.email "jorge.israel.p@gmail.com"
+
   git remote add upstream "https://$GH_TOKEN@github.com/blaenk/hoedown.git"
   git tag -a "v$version" -m "published https://crates.io/crates/hoedown/$version"
   git push upstream --tags
