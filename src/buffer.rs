21c3f5baa5c4d164c00b670c62a73bcc078ee349
Till Hoeppner <till@hoeppner.ws>
2015-02-21 21:17:54 +0100

    Update for changes to `std::ptr::Unique`. Contains lots of sed replacement, might be malformatted.

--- src/buffer.rs
+++ src/buffer.rs
@@ -27,7 +27,7 @@ impl Buffer {
     /// grow as more space is required.
     pub fn new(size: usize) -> Buffer {
         Buffer {
-            buffer: Unique(unsafe { hoedown_buffer_new(size as size_t) }),
+            buffer: unsafe { Unique::new(hoedown_buffer_new(size as size_t)) },
             is_owned: true,
         }
     }
@@ -47,19 +47,19 @@ impl Buffer {
         assert!(!buffer.is_null());
 
         Buffer {
-            buffer: Unique(buffer),
+            buffer: Unique::new(buffer),
             is_owned: false,
         }
     }
 
     /// Get a reference to the underlying buffer
     pub fn get<'a>(&'a self) -> &'a hoedown_buffer {
-        unsafe { &*self.buffer.ptr }
+        unsafe { & **self.buffer }
     }
 
     /// Get a mutable reference to the underlying buffer
     pub fn get_mut<'a>(&'a mut self) -> &'a mut hoedown_buffer {
-        unsafe { &mut *self.buffer.ptr }
+        unsafe { &mut **self.buffer }
     }
 
     /// Check if the buffer is empty
@@ -69,26 +69,26 @@ impl Buffer {
 
     /// The length of the contents inside the buffer
     pub fn len(&self) -> u64 {
-        unsafe { (*self.buffer.ptr).size }
+        unsafe { (**self.buffer).size }
     }
 
     /// Get a raw constant pointer to the buffer data
     pub fn as_ptr(&self) -> *const u8 {
-        unsafe { (*self.buffer.ptr).data }
+        unsafe { (**self.buffer).data }
     }
 
     /// Pipe another buffer's contents into this one
     pub fn pipe(&mut self, input: &Buffer) {
         unsafe {
-            hoedown_buffer_put(self.buffer.ptr, input.as_ptr(), input.len());
+            hoedown_buffer_put(*self.buffer, input.as_ptr(), input.len());
         }
     }
 
     /// Get a slice of the buffer's contents
     pub fn as_slice<'a>(&'a self) -> &'a [u8] {
         unsafe {
-            let data = (*self.buffer.ptr).data as *const u8;
-            let size = (*self.buffer.ptr).size as usize;
+            let data = (**self.buffer).data as *const u8;
+            let size = (**self.buffer).size as usize;
 
             mem::transmute(slice::from_raw_parts(data, size))
         }
@@ -97,8 +97,8 @@ impl Buffer {
     /// Get a mutable slice of the buffer's contents
     pub fn as_mut_slice<'a>(&'a mut self) -> &'a mut [u8] {
         unsafe {
-            let data = (*self.buffer.ptr).data;
-            let size = (*self.buffer.ptr).size as usize;
+            let data = (**self.buffer).data;
+            let size = (**self.buffer).size as usize;
 
             slice::from_raw_parts_mut(data, size)
         }
@@ -116,7 +116,7 @@ unsafe impl Send for Buffer {}
 impl Clone for Buffer {
     fn clone(&self) -> Buffer {
         // create a buffer with the same unit size
-        let unit = unsafe { (*self.buffer.ptr).unit };
+        let unit = unsafe { (**self.buffer).unit };
         let mut buffer = Buffer::new(unit as usize);
         // pipe this one's contents into it
         buffer.pipe(self);
@@ -133,7 +133,7 @@ impl Reader for Buffer {
 impl Writer for Buffer {
     fn write_all(&mut self, buf: &[u8]) -> IoResult<()> {
         unsafe {
-            hoedown_buffer_put(self.buffer.ptr, buf.as_ptr(), buf.len() as size_t);
+            hoedown_buffer_put(*self.buffer, buf.as_ptr(), buf.len() as size_t);
         }
         Ok(())
     }
@@ -142,7 +142,7 @@ impl Writer for Buffer {
 impl Drop for Buffer {
     fn drop(&mut self) {
         if self.is_owned {
-            unsafe { hoedown_buffer_free(self.buffer.ptr); }
+            unsafe { hoedown_buffer_free(*self.buffer); }
         }
     }
 }
