ed994413041ea403e3eedbc7bd3d981a6423861b
Jorge Israel Peña <jorge.israel.p@gmail.com>
2015-02-21 21:15:43 +0000

    rebuilding docs from a14bd36

--- implementors/core/marker/trait.Send.js
+++ implementors/core/marker/trait.Send.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['hoedown'] = ["<a class='stability Unstable' title='Unstable: will be overhauled with new lifetime rules; see RFC 458'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Send.html' title='core::marker::Send'>Send</a> for <a class='struct' href='hoedown/buffer/struct.Buffer.html' title='hoedown::buffer::Buffer'>Buffer</a>",];
+implementors['hoedown'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Send.html' title='core::marker::Send'>Send</a> for <a class='struct' href='hoedown/buffer/struct.Buffer.html' title='hoedown::buffer::Buffer'>Buffer</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
---
+++ implementors/std/io/trait.Read.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['hoedown'] = ["<a class='stability Unstable' title='Unstable: this new I/O module is still under active deveopment and APIs are subject to tweaks fairly regularly'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/io/trait.Read.html' title='std::io::Read'>Read</a> for <a class='struct' href='hoedown/buffer/struct.Buffer.html' title='hoedown::buffer::Buffer'>Buffer</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
