d1266dd460f326c30b50538a20626161f039d4c6
Jorge Israel Peña <jorge.israel.p@gmail.com>
2015-08-16 14:59:30 -0700

    unconditionally create git tag
    if the travis build passed, we want to create a git tag regardless of
    whether or not a crate was published
    
    then publish the crate only if the git tag didn't previously exist

--- .travis/publish_crate.sh
+++ .travis/publish_crate.sh
@@ -4,13 +4,10 @@ set -o errexit -o nounset
 
 ([[ $TRAVIS_BRANCH != "master" ]] || [[ $TRAVIS_PULL_REQUEST != "false" ]]) && exit 0
 
-cargo login $CRATESIO_TOKEN
+version=$(cat Cargo.toml | awk -F'"' '/version/{print $2}')
 
-if cargo publish; then
-  echo "Published new version on crates.io"
-
-  echo "Creating new git tag"
-  version=$(cat Cargo.toml | awk -F'"' '/version/{print $2}')
+if ! git rev-parse $version > /dev/null 2>&1; then
+  echo "Creating new git tag $version"
 
   git config user.name "Jorge Israel Peña"
   git config user.email "jorge.israel.p@gmail.com"
@@ -18,7 +15,16 @@ if cargo publish; then
   git remote add upstream "https://$GH_TOKEN@github.com/blaenk/hoedown.git"
   git tag -a "v$version" -m "published https://crates.io/crates/hoedown/$version"
   git push upstream --tags
-else
-  echo "Version already existed"
-fi
 
+  echo "Publishing to crates.io"
+
+  cargo login $CRATESIO_TOKEN
+
+  if cargo publish; then
+    echo "Published new version on crates.io"
+  else
+    echo "Error; Perhaps version already exists?"
+  fi
+else
+    echo "Tag $version already exists; no tag created, no crate published"
+fi
